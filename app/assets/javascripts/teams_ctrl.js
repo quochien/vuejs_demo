0238d276740ddd0f0295bc66c626f4ccf7adcd67
Hien Nguyen <quochien@gmail.com>
2017-12-12 22:40:08 +0700

    vuejs

---
+++ app/assets/javascripts/teams_ctrl.js
@@ -0,0 +1,8 @@
+$(document).on('ready', function() {
+  new Vue({
+    el: '#new-team-form',
+    data: {
+      message: 'Vue is working!'
+    }
+  })
+})
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'teams#new'
+  namespace :api do
+    namespace :v1 do
+      get '/teams' => 'teams#index'
+      post '/teams' => 'teams#create'
+    end
+  end
 end
